0ab5d092a43913e0160c8adf5e801b6a2aacca5e
Santiago Vargas <santiago.vargas@itti.digital>
2024-02-23 13:48:38 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test-database', function () {
+    try {
+        $result = DB::table('information_schema.tables')->exists();
+        return "Conexión a la base de datos establecida correctamente.";
+    } catch (\Exception $e) {
+        return "Error al conectar a la base de datos: " . $e->getMessage();
+    }
+});
